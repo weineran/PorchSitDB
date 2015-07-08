2bcf57931a3bf74f8fb2d726b9f2a9e81e655f6c
Andrew <weiner.andrew.j@gmail.com>
2015-07-08 09:45:33 -0500

    added database connection credential

--- include/DB_Connect.php
+++ include/DB_Connect.php
@@ -16,7 +16,7 @@ class DB_Connect {
 //        require_once 'include/Config.php';
         // connecting to mysql
         //$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD) or die(mysql_error());
-        $con = mysql_connect("localhost","root","") or die(mysql_error());
+        $con = mysql_connect("localhost","root","PorchSit") or die(mysql_error());
         // selecting database
         //mysql_select_db(DB_DATABASE) or die(mysql_error());
         mysql_select_db("PorchSit_database") or die(mysql_error());
